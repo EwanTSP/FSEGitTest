6861144b5c3f16fc90fccdfc4160051a358047bb
Ewan Chalmers <chalmersewan@gmail.com>
2021-11-18 15:37:58 +0000

    Person class first commit

---
+++ Person.java
@@ -0,0 +1,22 @@
+public class Person
+{
+
+private String	firstname;
+private String	othername;
+private String	surname;
+
+	public Person()
+	{
+		firstname = "";
+		othername = "";
+		surname = "";
+	}
+
+	public Person(String value1, String value2)
+	{
+		firstname = value1;
+		surname = value2;
+		othername = "";
+	}
+
+}
